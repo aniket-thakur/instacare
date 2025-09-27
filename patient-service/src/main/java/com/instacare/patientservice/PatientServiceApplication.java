668487345cc6d1a4777af2078139a609a6a928ba
Aniket Thakur <anikett126@gmail.com>
2025-09-27 14:32:20 +0530

    added dockerfile

---
+++ patient-service/src/main/java/com/instacare/patientservice/PatientServiceApplication.java
@@ -0,0 +1,15 @@
+package com.instacare.patientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class PatientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientServiceApplication.class, args);
+    }
+
+}
